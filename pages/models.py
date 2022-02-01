e391ec513d0638109a246f130625ababecc46fd7
Markos Amsalu <markosmuche2018@gmail.com>
2022-02-01 20:18:11 +0300

    Created Category, Auction, Bid, Comment and Watchlist models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from multiprocessing.spawn import import_main_path
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models import CASCADE
+from accounts.models import CustomUser
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=50, null=False)
+
+class Auction(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False, default='1')
+    title = models.CharField(max_length=50 , null=False, unique=True)
+    description = models.CharField(max_length=100, null=False, default='ABC')
+    starting_price = models.IntegerField(null=False , default='123')
+    category = models.ForeignKey(Category, on_delete=CASCADE, null=False, default='1')
+    active = models.BooleanField(null=False, default=False)
+
+
+class Bids(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=CASCADE, null=False, default='1')
+    auction = models.ForeignKey(Auction, on_delete=CASCADE, null=False, default='1')
+    bid_price = models.IntegerField(null=False, default='1000')
+    bid_time = models.TimeField(null=False, default='00:00:00')
+
+class Comments(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=CASCADE, null=False, default='1')
+    auction = models.ForeignKey(Auction, on_delete=CASCADE, null=False, default='1')
+    comments = models.CharField(max_length=150, default='abc')
+
+class watchlist(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=CASCADE,)
+    auction = models.ForeignKey(Auction, on_delete=CASCADE)
+    active = models.BooleanField(default=False)
