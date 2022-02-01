12ce5e3a4be852f328b51d8682e5714176365001
Markos Amsalu <markosmuche2018@gmail.com>
2022-02-01 21:45:48 +0300

    Admin page added

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from pages.models import Auction, Category
+
+
+class AuctionAdmin(admin.ModelAdmin):
+    
+    list_display = [field.name for field in Auction._meta.fields]
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = [field.name for field in Category._meta.fields]
+
+
+admin.site.register(Auction, AuctionAdmin)
+
+admin.site.register(Category, CategoryAdmin)
