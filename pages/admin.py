a67976ace745208a5adb88d0f22916df590d9c58
Markos Amsalu <markosmuche2018@gmail.com>
2022-02-02 13:18:39 +0300

    Admin page for sourcedata

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from pages.models import Auction, Category
+from pages.models import Auction, Category, DataSource
 
 
 class AuctionAdmin(admin.ModelAdmin):
@@ -11,7 +11,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
     list_display = [field.name for field in Category._meta.fields]
 
+class DatasourceAdmin(admin.ModelAdmin):
+
+    list_display = [field.name for field in DataSource._meta.fields]
+
 
 admin.site.register(Auction, AuctionAdmin)
 
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(DataSource, DatasourceAdmin)
