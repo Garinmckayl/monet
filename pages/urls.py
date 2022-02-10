6d552e7b55184f69087a70288c413fb10651ce52
Markos Amsalu <markosmuche2018@gmail.com>
2022-02-10 23:57:42 +0300

    Company create and detail view

--- pages/urls.py
+++ pages/urls.py
@@ -4,15 +4,13 @@ from accounts import views as account_views
 from django.urls import path, reverse
 
 from .views import (AboutPageView, AuctionCreateView, AuctionDetailView, AuctionListView, BidCreateView, BidDetailView,
-                    DashboardPageView, DatasourceView, HomePageView, StripeConnectionView,
+                    DashboardPageView, DatasourceView, HomePageView, StripeConnectionView,MyAuctionDetailView,
                     auction_create_success)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
-    path('dashboard/company/<int:pk>/', account_views.CompanyUpdateView.as_view(), name="company"),
-    path('dashboard/connect/', account_views.connect, name="connect"),
     path('auctions/', AuctionListView.as_view(), name='auctions'),
     path('auction-detail/<int:pk>/', AuctionDetailView.as_view(), name='auction-detail'),
     path('auctions/create/',AuctionCreateView.as_view(), name= 'auction-create'),
@@ -20,6 +18,12 @@ urlpatterns = [
     path('bid-detail/<int:pk>/', BidDetailView.as_view(), name='bid-detail'),
     path('auctions/create/success/', auction_create_success, name='auction_create_success'),
     path('stripe-connection/', StripeConnectionView.as_view(), name='stripe-connection'),
-    path('data-source',DatasourceView.as_view(), name='data-source'),
+    path('data-source/',DatasourceView.as_view(), name='data-source'),
+    path('dashboard/my-auction/', MyAuctionDetailView.as_view(),name='my-auction'),
+    
+    path('dashboard/company/my-company', account_views.MyCompanyDetailView.as_view(), name='my-company'),
+    path('dashboard/company/<int:pk>/', account_views.CompanyUpdateView.as_view(), name="company-update"),
+    path('dashboard/company/create/', account_views.CompanyCreateView.as_view(), name='company-create'),
+    path('dashboard/connect/', account_views.connect, name="connect"),
     
 ]
