01a65f423ad228fa39c31d9b77ee9ca5e45d5a49
Markos Amsalu <markosmuche2018@gmail.com>
2022-02-13 20:35:51 +0300

    debug here and ther

--- config/settings.py
+++ config/settings.py
@@ -177,8 +177,8 @@ ACCOUNT_FORMS = {
 'signup': 'accounts.forms.CustomUserCreationForm',
 }
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
 
-django_on_heroku.settings(locals())
+# django_on_heroku.settings(locals())
