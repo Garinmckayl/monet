6f92666be88ceb4c61a978041e5e82b9f235259d
Nathaniel Zeleke(高钨绩) <natnaelgetenew@gmail.com>
2022-02-16 08:54:11 -0800

    add django heroku

--- config/settings.py
+++ config/settings.py
@@ -180,3 +180,6 @@ ACCOUNT_FORMS = {
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+import django_heroku
+django_heroku.settings(locals())
